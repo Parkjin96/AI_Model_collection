56f2160032521a31f7be21288ec4350d5457a10e
JinWoo Park <54769819+Parkjin96@users.noreply.github.com>
2021-02-25 19:31:34 +0900

    Update Lgbm_base.py

--- Machine Learning/Lgbm_base.py
+++ Machine Learning/Lgbm_base.py
@@ -1,15 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
 import lightgbm as lgb
 from sklearn.model_selection import train_test_split
-
-
-# In[2]:
-
 
 # adjust param
 lgbm_param = {
@@ -25,10 +15,6 @@ lgbm_param = {
     'seed' : SEED,
 }
 
-
-# In[ ]:
-
-
 # train/valid data split
 X_train, X_vallid, y_train, y_valid = train_test_split(X, 
                                                     y,
@@ -37,15 +23,10 @@ X_train, X_vallid, y_train, y_valid = train_test_split(X,
                                                     random_state=seed)
 
 
-# In[ ]:
-
-
 #make dataset
 dtrain = lgb.Dataset(X_train, y_train)
 dvalid = lgb.Dataset(X_valid, y_valid)
 
-
-# In[ ]:
 
 
 # train model
@@ -53,14 +34,8 @@ model = lgb.train(lgbm_param , dtrain,  num_boost_round = num, verbose_eval = ve
                        valid_sets=(dtrain, dvalid), valid_names=('train','valid'))
 
 
-# In[ ]:
-
-
 # feature importance
 lgb.plot_importance(model, importance_type='gain', max_num_features = max_num_features)
-
-
-# In[ ]:
 
 
 # predict test data
