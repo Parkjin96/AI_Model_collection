4a0746ae00a28b2d823bae971be24ab214ead1b8
JinWoo Park <54769819+Parkjin96@users.noreply.github.com>
2021-03-12 23:12:18 +0900

    Add files via upload

---
+++ Deep Learning/Image + Time Series/ConvLSTM.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# 출처 : 
+
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from glob import glob
+from tqdm import tqdm
+
+import tensorflow as tf
+from tensorflow.keras.models import load_model, Model
+from tensorflow.keras.layers import Conv3D, ConvLSTM2D, BatchNormalization, Input
+from tensorflow.keras.optimizers import Adam
+
+
+train_dataset = tf.data.Dataset.from_tensor_slices((x_train_, y_train_))
+train_dataset = train_dataset.map(train_map_func, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+train_dataset = train_dataset.batch(BATCH_SIZE)
+train_dataset = train_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+val_dataset = tf.data.Dataset.from_tensor_slices((x_val_, y_val_))
+val_dataset = val_dataset.map(train_map_func, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+val_dataset = val_dataset.batch(BATCH_SIZE)
+val_dataset = val_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+
+
+
+
+
+inputs=Input((None, x_train_.shape[2], x_train_.shape[3], 1))
+
+convLSTM = ConvLSTM2D(filters=64, kernel_size=3, padding='same', return_sequences=True)(inputs)
+convLSTM = BatchNormalization()(convLSTM)
+
+convLSTM = ConvLSTM2D(filters=64, kernel_size=3, padding='same', return_sequences=True)(convLSTM)
+convLSTM = BatchNormalization()(convLSTM)
+
+convLSTM = ConvLSTM2D(filters=64, kernel_size=3, padding='same', return_sequences=True)(convLSTM)
+convLSTM = BatchNormalization()(convLSTM)
+
+outputs = Conv3D(filters=1, kernel_size=3, activation='relu', padding='same', data_format='channels_last')(convLSTM)
+
+model = Model(inputs, outputs)
+model.compile(loss='mae', optimizer=Adam())
+
+model.summary()
+
+
+
+
+
+
+
+callbacks_list = [
+    tf.keras.callbacks.ModelCheckpoint(
+        filepath = './models/convlstm_model.h5',
+        monitor='val_loss',
+        save_best_only=True
+    )
+]
+
+hist = model.fit(train_dataset, epochs=20, validation_data=val_dataset, callbacks=callbacks_list)
+
+
+
+
+
+plt.plot(hist.history['loss'][1:])
+plt.plot(hist.history['val_loss'][1:])
+plt.show()
+
+
+
+
+model = load_model('./models/convlstm_model.h5')
+
